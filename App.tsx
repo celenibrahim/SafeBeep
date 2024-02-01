2c4a7ed61c4cfedbfc6504569eae4d74600dfcb2
İbrahim ÇELEN <demontos67800@gmail.com>
2024-02-01 19:53:19 +0300

    First init for raw react-native app

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function SafeBeep(){
+  return(
+    <View>
+      <Text>
+        Hi, this is React-Native!
+      </Text>
+    </View>
+  )
+}
+export default SafeBeep;
