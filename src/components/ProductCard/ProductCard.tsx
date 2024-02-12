a0255cb6182620dd026de2345cc3ada47464f389
İbrahim ÇELEN <demontos67800@gmail.com>
2024-02-12 16:24:43 +0300

    added ProductCard custom component for printing products on the screen and raw json data file

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './ProductCard.style';
+const ProductCard = (props: {
+  item: {
+    id:
+      | string
+      | number
+      | boolean
+      | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+      | Iterable<React.ReactNode>
+      | React.ReactPortal
+      | null
+      | undefined;
+    product_name:
+      | string
+      | number
+      | boolean
+      | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+      | Iterable<React.ReactNode>
+      | React.ReactPortal
+      | null
+      | undefined;
+    price:
+      | string
+      | number
+      | boolean
+      | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+      | Iterable<React.ReactNode>
+      | React.ReactPortal
+      | null
+      | undefined;
+  };
+}) => {
+  return (
+    <View style={styles.container}>
+      <Text>{props.item.id}</Text>
+      <Text>{props.item.product_name}</Text>
+      <Text>{props.item.price}</Text>
+    </View>
+  );
+};
+
+export default ProductCard;
--- src/pages/Sell/Products/Products.tsx
+++ src/pages/Sell/Products/Products.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import styles from './Products.style';
-
+import products_data from '../../../products-data.json';
+import ProductCard from '../../../components/ProductCard';
 function Products() {
+  const renderProduct = ({item}: any) => <ProductCard item={item} />;
+
   return (
     <View>
-      <Text>Products Page!</Text>
+      <FlatList
+        keyExtractor={item => item.id}
+        data={products_data}
+        renderItem={renderProduct}
+      />
     </View>
   );
 }
