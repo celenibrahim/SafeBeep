a8d5787ab57f4fdcda3375467f71d58a2a3e0fed
İbrahim ÇELEN <demontos67800@gmail.com>
2024-04-18 03:37:04 +0300

    designed raw paypage and added buttons with inputbox

--- src/pages/Sell/PayPage/Pay.tsx
+++ src/pages/Sell/PayPage/Pay.tsx
@@ -1,14 +1,308 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
 
 const Pay = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleInput = (value: any) => {
+    setInput(input + value);
+  };
+  const handleDelete = () => {
+    setInput(input.slice(0, -1));
+  };
+
+  const clearInput = () => {
+    setInput('');
+    setResult('');
+  };
   return (
-    <View>
-      <Text>Pay</Text>
+    <View style={styles.container}>
+      <View style={styles.optContainer}>
+        <TouchableOpacity style={[styles.optButtons, {backgroundColor: 'red'}]}>
+          <Text style={styles.text_button}>Belge İptal</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.optButtons, {backgroundColor: '#ffa200'}]}>
+          <Text style={styles.text_button}>Belge Bitir</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.optContainer}>
+        <TouchableOpacity style={[styles.optButtons, {backgroundColor: 'red'}]}>
+          <Text style={styles.text_button}>Sil</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.optButtons, {backgroundColor: '#0098d9'}]}>
+          <Text style={styles.text_button}>Hadi Cüzdan</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.optContainer}>
+        <TouchableOpacity
+          style={[styles.optButtons, {backgroundColor: '#0098d9'}]}>
+          <Text style={styles.text_button}>TomBank</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.optButtons, {backgroundColor: '#15b82e'}]}>
+          <Text style={styles.text_button}>Hediye Çeki</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{flex: 1, backgroundColor: 'orange'}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            backgroundColor: 'gray',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.input}>{input}</Text>
+            <Text style={styles.result}>{result}</Text>
+          </View>
+          <View>
+            <TouchableOpacity style={styles.del_button} onPress={clearInput}>
+              <Text style={styles.del_button_text}>X</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={{flexDirection: 'row', flex: 1, margin: 5}}>
+          <View style={styles.buttonContainer}>
+            <View style={styles.row}>
+              <TouchableOpacity
+                style={styles.bot_top_buttons}
+                onPress={() => handleInput('00')}>
+                <Text style={styles.text_button}>00</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleInput('000')}>
+                <Text style={styles.text_button}>000</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.row}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleInput('1')}>
+                <Text style={styles.text_button}>1</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleInput('2')}>
+                <Text style={styles.text_button}>2</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleInput('3')}>
+                <Text style={styles.text_button}>3</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.row}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleInput('4')}>
+                <Text style={styles.text_button}>4</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleInput('5')}>
+                <Text style={styles.text_button}>5</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleInput('6')}>
+                <Text style={styles.text_button}>6</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.row}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleInput('7')}>
+                <Text style={styles.text_button}>7</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleInput('8')}>
+                <Text style={styles.text_button}>8</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleInput('9')}>
+                <Text style={styles.text_button}>9</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.row}>
+              <TouchableOpacity
+                style={styles.bot_top_buttons}
+                onPress={() => handleInput('0')}>
+                <Text style={styles.text_button}>0</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleInput('.')}>
+                <Text style={styles.text_button}>.</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <View>
+            <View style={styles.row}>
+              <TouchableOpacity
+                style={{
+                  marginLeft: 3,
+                  marginVertical: 2,
+                  borderWidth: 1,
+                  borderRadius: 10,
+                  width: Dimensions.get('window').width / 3,
+                  height: Dimensions.get('window').height / 11.5,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'darkblue',
+                }}
+                onPress={handleDelete}>
+                <Text style={styles.text_button}>Dell</Text>
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity
+              style={{
+                marginLeft: 3,
+                marginVertical: 2,
+                borderWidth: 1,
+                borderRadius: 10,
+                width: Dimensions.get('window').width / 3,
+                height: Dimensions.get('window').height / 5.6,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'green',
+              }}
+              onPress={() => null}>
+              <Text style={styles.text_button}>Credit Card</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={{
+                marginLeft: 3,
+                marginVertical: 2,
+                borderWidth: 1,
+                borderRadius: 10,
+                width: Dimensions.get('window').width / 3,
+                height: Dimensions.get('window').height / 5.6,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'green',
+              }}
+              onPress={() => null}>
+              <Text style={styles.text_button}>Cash</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default Pay;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: 'white'},
+  optContainer: {flexDirection: 'row'},
+  optButtons: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 3,
+    height: Dimensions.get('screen').height / 14,
+    width: Dimensions.get('screen').width / 2,
+    borderWidth: 1,
+    borderRadius: 8,
+    margin: 5,
+  },
+  text_button: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    margin: 2,
+    borderWidth: 1,
+    borderRadius: 10,
+    width: Dimensions.get('window').width / 5,
+    height: Dimensions.get('window').height / 11.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'darkblue',
+  },
+  del_button: {
+    marginLeft: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    width: Dimensions.get('window').width / 5,
+    height: Dimensions.get('window').height / 11.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  del_button_text: {
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  bot_top_buttons: {
+    marginHorizontal: 2,
+    marginVertical: 2,
+    borderWidth: 1,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'darkblue',
+    width: Dimensions.get('window').width / 2.41,
+  },
+  buttonContainer: {
+    width: '70%',
+    flex: 1,
+  },
+  inputContainer: {
+    borderWidth: 1,
+    borderRadius: 10,
+    marginBottom: 20,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  input: {
+    width: Dimensions.get('window').width / 1.5,
+    borderWidth: 1,
+    fontSize: 24,
+    margin: 10,
+    borderRadius: 10,
+  },
+  result: {
+    textAlign: 'center',
+    borderRadius: 10,
+    fontSize: 24,
+    fontWeight: 'bold',
+    borderWidth: 1,
+    marginBottom: 10,
+    width: Dimensions.get('window').width / 1.5,
+  },
+});
